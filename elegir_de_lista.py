d41c808ed7be3d08390297aa4067454bca735daf
Andres <MG>
2023-05-12 09:13:02 +0200

    type hints

--- elegir_de_lista.py
+++ elegir_de_lista.py
@@ -2,8 +2,7 @@
 import random
 from pide_num import pideUnNum 
 
-
-def elegir_indice(lista):
+def elegir_indice(lista: list) -> int:
     ver_lista(lista)
     indice_lista = -1
     while indice_lista not in range(len(lista)):
@@ -12,20 +11,17 @@ def elegir_indice(lista):
 
     return int(indice_lista)
 
-def ver_lista(lista):
+def ver_lista(lista: list) -> None:
     indice_pantalla = 0
     for i in lista:
         print(f"{indice_pantalla} - {i}")
         indice_pantalla += 1 
 
-def elemento_lista(lista, indice_lista):
+def elemento_lista(lista: list, indice_lista: int) -> None:
     elemento_lista = (f"{lista[indice_lista]} es el elemento {indice_lista}")
     print(elemento_lista)
 
-    # return elemento_lista
-
-
-def elegir_aleatorio(lista):
+def elegir_aleatorio(lista: list) -> int:
     indice_aleatorio = random.randrange(len(lista))
 
     return indice_aleatorio
